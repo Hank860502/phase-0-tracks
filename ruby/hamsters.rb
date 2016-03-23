9d27133319db968d7a0c3f2474bfc6d26947112a
Hank860502 <hank860502@gmail.com>
2016-03-23 12:11:11 +0800

    4.3 hamster qusetions

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+puts "What is the hamster's name?"
+name = gets.chomp
+puts "How loud is the hamster is? From 1-10."
+volume = gets.chomp
+volume.to_i
+puts "What is the hamster's fur color?"
+color = gets.chomp
+puts "Is the hamster is a good candidate for adoption(x/o)?"
+adoption = gets.chomp
+def x = false
+	end 
+def o = true
+	end
+puts "Estimated age?"
+age = get.chomp
