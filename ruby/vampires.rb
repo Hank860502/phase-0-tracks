26dc389e882f4b8597de752a62054e9000fd2690
Hank860502 <hank860502@gmail.com>
2016-03-24 10:25:43 +0800

    release 2 try the thing

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -6,14 +6,21 @@ puts 'What is your name?'
 name = gets.chomp
 puts 'How old are you?'
 age = gets.chomp
-if age > 150
+if #{age} > 150
 	puts 'You are a vampire!'
 else 'You do not look like a vampire.'
 end
 puts 'Where were you born?'
 place = gets.chomp
-if place 
 puts 'Our company cafeteria serves garlic bread. Should we order some for you?'
 bread = gets.chomp
 puts 'Would you like to enroll in the company’s health insurance?'
 prefer = gets.chomp
+wolves_like_garlic && wolves_like_sunshine   #true
+wolves_like_garlic || vampires_like_garlic	#true
+wolves_like_garlic && vampires_like_garlic	#false
+wolves_like_garlic && (vampires_like_sunshine || vampires_like_garlic)	#false
+(wolves_like_garlic && vampires_like_garlic) || wolves_like_sunshine	#true
+vampires_like_garlic	#false
+!vampires_like_garlic	#true
+!(wolves_like_sunshine && wolves_like_garlic)	#false
