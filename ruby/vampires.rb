053fa89740140ae3eb841ce02cfcf98b503e0f0d
Hank860502 <hank860502@gmail.com>
2016-03-24 12:05:43 +0800

    add first linit to recognize employee

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,26 +1,18 @@
-wolves_like_sunshine = true
-wolves_like_garlic = true
-vampires_like_sunshine = false
-vampires_like_garlic = false
 puts 'What is your name?'
 name = gets.chomp
 puts 'How old are you?'
-age = gets.chomp
-if #{age} > 150
-	puts 'You are a vampire!'
-else 'You do not look like a vampire.'
-end
+age = gets.chomp.to_i
 puts 'Where were you born?'
 place = gets.chomp
-puts 'Our company cafeteria serves garlic bread. Should we order some for you?'
-bread = gets.chomp
-puts 'Would you like to enroll in the company’s health insurance?'
-prefer = gets.chomp
-wolves_like_garlic && wolves_like_sunshine   #true
-wolves_like_garlic || vampires_like_garlic	#true
-wolves_like_garlic && vampires_like_garlic	#false
-wolves_like_garlic && (vampires_like_sunshine || vampires_like_garlic)	#false
-(wolves_like_garlic && vampires_like_garlic) || wolves_like_sunshine	#true
-vampires_like_garlic	#false
-!vampires_like_garlic	#true
-!(wolves_like_sunshine && wolves_like_garlic)	#false
+puts 'Our company cafeteria serves garlic bread. Should we order some for you(y/n)?'
+bread = gets.chomp.to_s
+y=true
+n=false
+puts 'Would you like to enroll in the company’s health insurance(y/n)?'
+c = gets.chomp
+
+if age < 150 && bread = y #not sure yet
+	puts 'Probably not a vampire.'
+else
+	puts 'You are a vampire!'
+end
