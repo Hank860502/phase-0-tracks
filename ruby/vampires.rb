e344ed1344d83f8bde41e8c7b6c71fa1ebb09caa
Hank860502 <hank860502@gmail.com>
2016-03-24 12:54:00 +0800

    set three condition not including name

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,18 +1,18 @@
 puts 'What is your name?'
-name = gets.chomp
+name = gets.chomp.to_s
 puts 'How old are you?'
 age = gets.chomp.to_i
 puts 'Where were you born?'
 place = gets.chomp
 puts 'Our company cafeteria serves garlic bread. Should we order some for you(y/n)?'
 bread = gets.chomp.to_s
-y=true
-n=false
 puts 'Would you like to enroll in the company’s health insurance(y/n)?'
-c = gets.chomp
+c = gets.chomp.to_s
 
-if age < 150 && bread = y #not sure yet
+if age < 150 && (bread == "y" || c == "y")  #not sure yet
 	puts 'Probably not a vampire.'
-else
-	puts 'You are a vampire!'
+elsif age > 150 && (bread == "n" || c == "n") 	#not sure yet
+	puts 'Probably a vampire.'
+else 
+	puts 'Results inconclusive.'
 end
