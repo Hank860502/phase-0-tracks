26c3f6567811425ee5a485b6c0cc55da0ce650bb
Hank860502 <hank860502@gmail.com>
2016-03-26 22:35:46 +0800

    Understand the full concept and add more comment

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -3,19 +3,18 @@
 #store the result in the current index
 #return updated string
 
-def encrypt(var)
+def encrypt(var)  #var=random string.
    store = ''
-   len = var.length
-   count = 0 
-   full = "abcdefghijklmnopqrstuvwxyz"
-   while count < len
-       if var[count].next == "aa" 
-           store << "a"
+   len = var.length   #prepare for line 10
+   count = 0  #represent the first word in a string, just like index.
+   while count < len  #loop through each character at a time until every words have been looped.
+       if var[count].next == "aa"  
+           store << "a"    #Normally the word after a is aa, but I want only a.
        else
-           store << var[count].next
-       end    
+           store << var[count].next   #first time looping through will be var[0].next 
+       end                            #and second time looping through become var[1].next etc.
        #store << full[full.index(var[count])+1]
-       count += 1
+       count += 1   #go on to the next index.
 
    end
    puts store
@@ -28,17 +27,17 @@ end
 #store the result in the current index
 #return updated string
 
-def decrypt(var)
+def decrypt(var)   #var=random string.
    store = ''
-   len = var.length
-   count = 0
-   full = "abcdefghijklmnopqrstuvwxyz"
+   len = var.length   #prepare for line 35
+   count = 0           #represent the first word in a string, just like index.
+   full = "abcdefghijklmnopqrstuvwxyz"  #prepare for line 38, so the code knows the previous word of a word. For example: b-1=a
    while count < len
        #index = count
        #store << (var[index].ord-1).chr
-       store << full[full.index(var[count])-1]
-       count += 1
-   end
+       store << full[full.index(var[count])-1]    #var(0) return the inex 0 word in a string. So return string. WHY WE DON'T NEED "" OUTSIDE STRING?
+       count += 1                                 #full.index(var[count]) finds string index in full. So return an index(number).
+   end                                            #full[full.index(var[count])-1]  minus 1 index to the index that is found in previous step.
    puts store
 end
 
@@ -68,3 +67,7 @@ pass = gets.chomp
 end
 
 puts prompt
+
+
+#pair with genius Dmitry
+#line 38 question of why don't we need "" for string
