a35fe7da1d774bd214d064f3a306f6800920f9c5
Hank860502 <hank860502@gmail.com>
2016-04-04 00:06:36 +0800

    5.4 done

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,15 +3,11 @@
 #change all the vowels in the name to next vowel 
 #change all of the consonants (everything else besides the vowels) to the next consonant in the alphabet.
 loop do 
-	user_info = {real_name: '', fake_name: ''}
 	puts 'What is your first name?'
 	user_first_name = gets.chomp.to_s
 	puts 'What is your last name?'
 	user_last_name = gets.chomp.to_s
-	
 	user_real_name = user_first_name + ' ' + user_last_name  #store real name
-	user_info[:real_name] = user_real_name   #store real name to the data structure.
-	puts user_info[:real_name]
 	user_name = user_last_name + ' ' + user_first_name  #to swap forst and last name
 	#user_name = user_name.split(' ') #this provide an array : ["Hank", "Chen"] so I can swap them. But terminal display method + doe not exist so I go another way.
 	#first_name = user_name[0]
@@ -40,18 +36,30 @@ loop do
 			end
 			count+=1
 		end
-		puts name
+		return name
 	end
-	puts vowel_machine(user_name) 
-	user_fake_name = vowel_machine(user_name)
-	user_info[:fake_name] = user_fake_name  #store fake name result to user info
-	puts user_info
+	p vowel_machine(user_name)
+	store_result = vowel_machine(user_name)
 	puts 'To exit, type quit.'
 	break if user_first_name == 'quit' || user_last_name == 'quit'
+	#puts "#{store_result} is acually #{store}!" will success here but display everytime a user type name.
+	#count = 0 #index
+	user_info = {}
+	user_info.store(user_real_name, store_result)
+	#user_info[:real_name].push(user_real_name)   #store real name to the data structure.
+	#store = user_info[:real_name]
+	#user_info[:fake_name].push(store_result)
+	#p user_info
+	puts "#{store_result} is acually #{user_real_name}"
+	p user_info
 end
 
+	#count+=1
+	#puts "#{store_result} is acually #{store}!" does not have the value of fake_name and real_name
 
 #extra line display in the result
+
+#def reveal_method(name, fake_name)
 
 
 
