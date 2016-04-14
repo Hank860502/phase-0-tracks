df9c4fe20f3be2fe1f956bb5e253139c292d038c
Hank860502 <hank860502@gmail.com>
2016-04-14 17:03:16 +0800

    pull missing repo js and databases from github

---
+++ js/explore.js
@@ -0,0 +1,10 @@
+// function that take a string as parameters and reverse it
+// "hello" => "olleh"
+// set i equals to length of hello, which is 5
+// loop until i = 0
+//count i from the last to the first index
+// print from the last to the first index
+var str = "hello"
+for (var i = str.length; i >= 0; i -=1){
+	console.log(str[i])
+}
