35a01a10d8d2b3be554bb89ce2c6503c9dad1429
Hank860502 <hank860502@gmail.com>
2016-04-16 14:11:13 +0800

    pseudocode first function

---
+++ js/algos.js
@@ -0,0 +1,9 @@
+// write a function that takes an array of words or phrases and returns the longest word in the array. 
+// "some string".length will do what you expect in JS.
+// create a function name longest and take a argument in a peremter.
+// creat a array name dictionary contains three words. dinosaur, cow, butterfly.
+// iterate through in the function to count the length of each word.  dictionary[i].length
+// store the length and compare the length of each word 
+// grasp the longest word
+// create a object holding word as key and length of word as value
+// after find the biggest length, call the value's key
