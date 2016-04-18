46db1ebfb6238935e33fc624b103339228564d74
Hank860502 <hank860502@gmail.com>
2016-04-18 10:10:33 +0800

    two function done, random_machune not done

--- js/algos.js
+++ js/algos.js
@@ -6,25 +6,19 @@
 // iterate through in the function to count the length of each word.  dictionary[i].store_length
 // grasp the longest word}
 function longest(array){
-	if (array[0].length > array[1].length && x.length > array[2].length){
-		console.log(array[0]);
+	var store_longest =array[0]
+	for (var i=1; i<array.length; i++){
+		if (array[i].length > store_longest.length) {
+			store_longest = array[i]
+		}
 	}
-	else if (array[1].length > array[0].length && array[1].length > array[2].length){
-		console.log(array[1]);
-	}
-	else if (array[2].length>array[0].length && array[2].length > array[1].length){
-		console.log(array[2]);
-	}
-	else{
-		console.log("error");
-	};
+	return (store_longest)
+}
 	// Store all the length in one place. I want to compare the value of each index and grasp the largest.
 	// google how to compare numbers' value
 	// google how to find the largest number
 	// console.log(largest number), something like this
-}
-var dictionary = ["eating", "shopping", "running"]
-longest(dictionary)
+
 // =================================
 // write a function that takes two objects and checks to see if the objects share any key-value pair.
 // create two objects that have keys in common
@@ -32,22 +26,34 @@ longest(dictionary)
 
 // function searching(object1, object2)
 function searching(object1, object2){
-	// for (var i = 0; i<object1.length; i ++){
-	console.log(object1)
-	console.log(object2)
-	console.log(Object.keys(object1)[0])  //all the keys in object1
-	console.log(Object.keys(object2))
-	console.log(object1[Object.keys(object1)[0]]) //access the index 0 value in object 1
-	// if (Object.keys(object1)[0] === Object.keys(object2)[0] && object1[Object.keys(object1)[0]] === object1[Object.keys(object2)[0]])
-	// 	{console.log(true)};
-	// else if (Object.keys(object1)[1] === Object.keys(object2)[1] && object1[Object.keys(object1)[1]] === object1[Object.keys(object2)[1]])
-	// 	{console.log("true")};
-	// else{
-	// 	(console.log("false"));
-	// };
+	for (var value in object1) {
+    if (object1[value] == object2[value]){
+    	return true;
+    }
 }
-var user1 = {name: 'Hank', age: '18'}
-var user2 = {name: 'Claire', age: '18'}
-searching(user1, user2)
+    	return false;  //when I code: else return false, the output is not correct,but with this, it works.
+}
+function stringMachine(number){
+	var array = [];
+    while (number >0){
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    for( var i=0; i < 10; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    array.push(text);
+    number -=1
+}
+ console.log(array)
+}
+
+
+
+var dictionary = ["eating", "shopping", "running"]
+console.log(longest(dictionary))
+var user1 = {name: 'Hank', age: 18}
+var user2 = {name: 'Han', age: 18}
+console.log(searching(user1, user2))
+stringMachine(5)
+
 
 
