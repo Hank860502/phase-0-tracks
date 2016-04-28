2c3dc89000d8364ad3ee5ea4519d0983bb4f9db4
Hank860502 <hank860502@gmail.com>
2016-04-28 09:41:36 -0700

    Building web application

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -23,6 +23,18 @@ get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
 
+get '/contact' do
+  "I live in 1600 Pennsylvania Ave NW, Washington, DC 20500"
+end
+
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job #{name}!"
+  else
+    "Good job!"
+  end
+end
 # write a GET route that retrieves
 # all student data
 get '/students' do
